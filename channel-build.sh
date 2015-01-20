d85b9c021f44d89ed67c3e3237ce9e2ce6a577f1
Graeme Coupar <grambo@grambo.me.uk>
2015-01-20 00:22:12 +0000

    Add builder.sh to channel .tar file.

--- channel-build.sh
+++ channel-build.sh
@@ -1,4 +1,4 @@
 nix-push --url-prefix http://plug.grambo.me.uk/plexmediaserver --manifest --dest /tmp/cache $(nix-build default.nix)
 mkdir -p channel
-tar --xz -cf channel/nixexprs.tar.xz default.nix
+tar --xz -cf channel/nixexprs.tar.xz default.nix builder.sh
 cp /tmp/cache/MANIFEST.bz2 channel
